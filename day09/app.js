bd3372ca1bf6b8c1f5de059d5a29537faf2067af
ibrahim-7869 <majidibrahim573@gmail.com>
2026-05-03 15:03:03 +0500

    feat(day09): create basic node.js script for containerization lab

---
+++ day09/app.js
@@ -0,0 +1,5 @@
+const os = require('os');
+console.log(`[INT] Custom Node Container Executing.`);
+console.log(`[INFO] Architecture: ${os.arch()}`);
+console.log(`[INFO] Hostname: ${os.hostname()}`);
+console.log(`[STATUS] Application  deployed successfully.`);
